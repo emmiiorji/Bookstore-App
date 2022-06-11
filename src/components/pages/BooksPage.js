6ebaabbf48011f470954f63779497c66805c5630
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-12 00:13:40 +0100

    Combine necessary components into the HomePage component

---
+++ src/components/pages/BooksPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AllBooks from '../AllBooks';
+import AddBookForm from '../AddBookForm';
+
+const BooksPage = () => (
+  <div>
+    <AllBooks />
+    <AddBookForm />
+  </div>
+);
+
+export default BooksPage;
