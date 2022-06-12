98f3d9fec3671e5f38a8db577f54504b27dbc9c1
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-12 01:54:59 +0100

    Add check status button on Categories page component

--- src/components/pages/CategoriesPage.js
+++ src/components/pages/CategoriesPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const CategoriesPage = () => (
-  <></>
+  <div>
+    <h1>Categories</h1>
+    <button type="button">Check status</button>
+  </div>
 );
 
 export default CategoriesPage;
