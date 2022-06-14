64b8b739822a38da0bcdb8c7fa4bebf24e2e871d
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-14 19:01:22 +0100

    Combine reducers into root reducer

---
+++ src/redux/configureStore.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import books from './books/books';
+import categories from './categories/categories';
+
+const rootReducer = combineReducers({
+  categories,
+  books,
+});
