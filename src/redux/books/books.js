ee26c49a075cb1270ec3fc25a32be9ed1db0cbe1
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-14 18:57:00 +0100

    Add the book reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,7 +2,22 @@
 const ADD = 'bookstore/book/ADD';
 const REMOVE = 'bookstore/book/remove';
 
+// Reducer
+const reducer = (state = [], action) => {
+  const { payLoad } = action;
+  switch (action.type) {
+    case ADD:
+      return [...state, payLoad.book];
+    case REMOVE:
+      return state.filter((book) => book.id !== payLoad.id);
+    default:
+      return state;
+  }
+};
+
 // Action Creators
 export const addBook = (book) => ({ type: REMOVE, payLoad: { book } });
 
 export const removeBook = (id) => ({ type: ADD, payLoad: { id } });
+
+export default reducer;
