36cd3ff79fd15d6813b18475c83777442dc3a31b
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-14 18:59:39 +0100

    Add dummy categories reducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,5 +1,17 @@
 // Actions
 const CHECK = 'bookstore/categories/CHECK';
 
+// Reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case CHECK:
+      return 'Under Construction';
+    default:
+      return state;
+  }
+};
+
 // Action Creators
 export const checkCategory = () => ({ type: CHECK });
+
+export default reducer;
