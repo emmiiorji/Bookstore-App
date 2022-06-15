404f2ea5137ea7e5232e9c3cbf4f932f4e003aa5
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-15 13:53:28 +0100

    Pass default books to the books reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,8 +4,29 @@ const Actions = {
   REMOVE: 'bookstore/book/REMOVE',
 };
 
+const stateInit = [
+  {
+    id: 1,
+    topic: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: 2,
+    topic: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    topic: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
+
 // Reducer
-const reducer = (state = [], action) => {
+const reducer = (state = stateInit, action) => {
   const { payLoad } = action;
   switch (action.type) {
     case Actions.ADD:
