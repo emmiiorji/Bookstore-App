c129d1124818ec34bda9f0f38753f7e18897791d
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-15 18:17:30 +0100

    Consume state from Redux redux store by reading the books key

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 import Book from './Book';
 
 const Books = () => {
-  const booksData = [
-    {
-      id: 1,
-      topic: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: 2,
-      topic: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      id: 3,
-      topic: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-    },
-  ];
+  const books = useSelector((state) => state.books, shallowEqual);
 
   return (
     <div>
-      {booksData.map((book) => (
+      {books.map((book) => (
         <Book topic={book.topic} title={book.title} author={book.author} key={book.id} />
       ))}
     </div>
