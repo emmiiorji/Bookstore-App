e85dca513c79193ddeb0ef14843119eddbce5932
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-16 06:37:29 +0100

    Set add book form fields as required

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -5,9 +5,9 @@ const AddBookForm = ({ handleSubmit }) => (
   <div className="addBookContainer">
     <h2>Add New Book</h2>
     <form name="addBook" onSubmit={handleSubmit}>
-      <input type="text" placeholder="Book title" />
-      <input type="text" placeholder="Book author" />
-      <select name="category" className="inputs">
+      <input type="text" placeholder="Book title" required />
+      <input type="text" placeholder="Book author" required />
+      <select name="category" className="inputs" required>
         <option value="" defaultValue>Category</option>
         <option value="Action">Action</option>
         <option value="Science Fiction">Science Fiction</option>
