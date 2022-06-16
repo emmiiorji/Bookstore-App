e76b4662fc8242cc8b0262edda98a649f1242422
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-16 06:46:43 +0100

    Initialize local react state for AddBookForm

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const AddBookForm = ({ handleSubmit }) => (
-  <div className="addBookContainer">
-    <h2>Add New Book</h2>
-    <form name="addBook" onSubmit={handleSubmit}>
-      <input type="text" placeholder="Book title" required />
-      <input type="text" placeholder="Book author" required />
-      <select name="category" className="inputs" required>
-        <option value="" defaultValue>Category</option>
-        <option value="Action">Action</option>
-        <option value="Science Fiction">Science Fiction</option>
-        <option value="Economy">Economy</option>
-      </select>
-      <button type="submit" className="add-btn">Add Book</button>
-    </form>
-  </div>
-);
+const AddBookForm = () => {
+  const dispatch = useDispatch();
+
+  const [book, setBook] = useState({
+    id: '',
+    title: '',
+    author: '',
+    category: '',
+  });
+
+  return (
+    <div className="addBookContainer">
+      <h2>Add New Book</h2>
+      <form name="addBook" onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Book title" required />
+        <input type="text" name="author" placeholder="Book author" required />
+        <select name="category" className="inputs" required>
+          <option value="" defaultValue>Category</option>
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button type="submit" className="add-btn">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 AddBookForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
