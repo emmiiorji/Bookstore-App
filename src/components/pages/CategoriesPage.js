05ff41bb9e2b176a11ed0f284ae12c9f32f100cd
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-17 07:27:31 +0100

    Dispatch action for check status

--- src/components/pages/CategoriesPage.js
+++ src/components/pages/CategoriesPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkCategory } from '../../redux/categories/categories';
 
-const CategoriesPage = () => (
-  <div>
-    <h1>Categories</h1>
-    <button type="button">Check status</button>
-  </div>
-);
+const CategoriesPage = () => {
+  const dispatch = useDispatch();
+
+  const status = useSelector((state) => state.categories);
+  return (
+    <div>
+      <h1>Categories</h1>
+      <button type="button" onClick={() => dispatch(checkCategory())}>Check status</button>
+      <p className="status">{status}</p>
+    </div>
+  );
+};
 
 export default CategoriesPage;
