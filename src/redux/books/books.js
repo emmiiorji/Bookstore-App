976f6c6f909d30f17ffc3917518f6d0181d30acc
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-17 10:33:54 +0100

    Remove default books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,34 +1,10 @@
-import { v4 as uuidv4 } from 'uuid';
-
 // Actions
 const Actions = {
   ADD: 'bookstore/book/ADD',
   REMOVE: 'bookstore/book/REMOVE',
 };
 
-const stateInit = [
-  {
-    id: uuidv4(),
-    topic: 'Action',
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    category: 'Action',
-  },
-  {
-    id: uuidv4(),
-    topic: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert',
-    category: 'Economy',
-  },
-  {
-    id: uuidv4(),
-    topic: 'Economy',
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    category: 'Science Fiction',
-  },
-];
+const stateInit = [];
 
 // Reducer
 const reducer = (state = stateInit, action) => {
