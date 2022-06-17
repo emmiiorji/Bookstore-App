ee04974b00cbd1c4ea4bbd72f3a520afc43586c3
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-17 06:33:05 +0100

    Remove commented code

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-// import ShowProgress from './ShowProgress';
 import { removeBook } from '../redux/books/books';
 
 const Book = (props) => {
@@ -22,22 +21,14 @@ const Book = (props) => {
         <button type="button" className="reaction" onClick={() => dispatch(removeBook(id))}>Remove</button>
         <button type="button" className="reaction">Edit</button>
       </div>
-      {/* <ShowProgress props={progress} /> */}
     </div>
   );
 };
-
-// const progressShape = {
-//   completedPercent: PropTypes.number,
-//   currentChapter: PropTypes.string,
-//   handleUpdateProgress: PropTypes.func,
-// };
 
 Book.propTypes = {
   id: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  // progress: PropTypes.shape(progressShape).isRequired,
 };
 export default Book;
