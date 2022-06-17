5e02d23c6fb67a18fe6c6ba059c6e6af9ca3f03c
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-17 10:46:45 +0100

    Dispatch loadBooks action on mounting of the Books component

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import Book from './Book';
+import { loadBooks } from '../redux/books/books';
 
 const Books = () => {
   const books = useSelector((state) => state.books, shallowEqual);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadBooks());
+  }, []);
 
   return (
     <div>
@@ -12,8 +18,8 @@ const Books = () => {
           topic={book.topic}
           title={book.title}
           author={book.author}
-          key={book.id}
-          id={book.id}
+          key={book.item_id}
+          id={book.item_id}
           category={book.category}
         />
       ))}
