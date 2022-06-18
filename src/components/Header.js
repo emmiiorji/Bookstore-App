6bd14cfb573d09e2151bbd53c3c1a9168692d446
emmiiorji <emmanuelorji2013@gmail.com>
2022-06-18 11:19:25 +0100

    Enclose the page's heading and navs into a div

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import { CgProfile } from 'react-icons/cg';
 import Navs from './Navs';
 
 const Header = () => (
-  <header>
-    <h1>Book Store CMS</h1>
-    <Navs />
+  <header className="header">
+    <div id="header-left">
+      <h1>Book Store CMS</h1>
+      <Navs />
+    </div>
     <CgProfile />
   </header>
 );
